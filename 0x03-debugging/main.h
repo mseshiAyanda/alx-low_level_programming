d98a002c467dcc101f98fc9877ad5ada8c97e195
mseshiAyanda <mseshidladla@gmail.com>
2022-07-07 11:15:33 -0700

    main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,4 +6,10 @@
 
 void positive_or_negative(int i);
 
+int largest_number(int, int, int);
+
+void print_remaining_days(int, int, int);
+
+int convert_day(int month, int day);
+
 #endif /* MAIN_H */
