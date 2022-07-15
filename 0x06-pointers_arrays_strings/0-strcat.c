46e5d13b931d5687a0f4553cc3801dc27cd3aab5
mseshiAyanda <mseshidladla@gmail.com>
2022-07-15 01:51:01 -0700

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,24 +8,43 @@
 */
 
 char *_strcat(char *dest, char *src)
-{
-
-int length, j;
-length = 0;
-
-while (dest[length] != '\0')
 
 {
-length++;
-}
 
-for (j = 0; src[j] != '\0'; j++, length++)
+		int i, j;
 
-{
-dest[length] = src[j];
-}
 
-dest[length] = '\0';
 
-return (dest);
+			i = 0;
+
+				j = 0;
+
+
+
+					while (dest[i] != '\0')
+
+								i++;
+
+
+
+						while (src[j] != '\0')
+
+								{
+
+											dest[i] = src[j];
+
+													j++;
+
+															i++;
+
+																}
+
+
+
+							dest[i] = '\0';
+
+
+
+								return (dest);
+
 }
