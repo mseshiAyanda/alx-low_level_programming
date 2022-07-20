cdec43a8451afa9da43c0f0a7ae5b54a2fcf3595
Ayanda <68080779+mseshiAyanda@users.noreply.github.com>
2022-07-20 22:55:14 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,20 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * main - check the code for ALX School students.
- * Return: Always 0.
+ *_puts_recursion - prints a string
+ *@s: pointer block of memory to fill
+ *Return: void
  */
-int main(void)
+
+void _puts_recursion(char *s)
+
 {
-    _puts_recursion("Betty Holberton");
-    return (0);
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+
+_putchar (*s);
+_puts_recursion(s + 1);
+
 }
